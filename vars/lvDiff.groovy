0cc282cb8fef1ff651bb8f82a8bc3792601f9051
Ryan Zoeller <ryan.zoeller@ni.com>
2018-10-24 10:25:18 -0500

    Use local paths for scripts instead of L:\

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -6,7 +6,7 @@ def call(lvVersion, diffingPicRepo = null) {
     def diffDir = "${WORKSPACE}\\diff_dir"
     bat "if exist ${diffDir} rd /s /q ${diffDir}"
     bat "mkdir ${diffDir}"
-    bat "git difftool --no-prompt --extcmd=\"'L:\\labview-diff.bat' \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
+    bat "git difftool --no-prompt --extcmd=\"'labview-diff.bat' \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
     // Silencing echo so as to not print out the token.
-    bat "@python L:\\github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME} --pic-repo=${diffingPicRepo}"
+    bat "@python github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME} --pic-repo=${diffingPicRepo}"
 }
