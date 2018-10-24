b0181af3860328f07dd6459a2b6affdadc9f9b3c
Ryan Zoeller <ryan.zoeller@ni.com>
2018-10-24 14:20:54 -0500

    Add path relative to workspace

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -4,9 +4,10 @@ def call(lvVersion, diffingPicRepo = null) {
         diffingPicRepo = env.DIFFING_PIC_REPO
     }
     def diffDir = "${WORKSPACE}\\diff_dir"
+    def stepsDir = "${WORKSPACE}\\buildsystem\\steps"
     bat "if exist ${diffDir} rd /s /q ${diffDir}"
     bat "mkdir ${diffDir}"
-    bat "git difftool --no-prompt --extcmd=\"'labview-diff.bat' \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
+    bat "git difftool --no-prompt --extcmd=\"'${stepsDir}\\labview-diff.bat' \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
     // Silencing echo so as to not print out the token.
-    bat "@python github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME} --pic-repo=${diffingPicRepo}"
+    bat "@python ${stepsDir}\\github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME} --pic-repo=${diffingPicRepo}"
 }
