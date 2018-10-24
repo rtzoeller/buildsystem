dce8c30e4c5531c92a7da57dfabbbb712d068270
Ryan Zoeller <ryan.zoeller@ni.com>
2018-10-24 14:34:00 -0500

    Quote args to capture spaces

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -9,5 +9,5 @@ def call(lvVersion, diffingPicRepo = null) {
     bat "mkdir ${diffDir}"
     bat "git difftool --no-prompt --extcmd=\"'${stepsDir}\\labview-diff.bat' \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
     // Silencing echo so as to not print out the token.
-    bat "@python ${stepsDir}\\github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME} --pic-repo=${diffingPicRepo}"
+    bat "@python '${stepsDir}\\github_commenter.py' --token='${GITHUB_DIFF_TOKEN}' --pic-dir='${diffDir}' --pull-req='${CHANGE_ID}' --info='${JOB_NAME}' --pic-repo='${diffingPicRepo}'"
 }
