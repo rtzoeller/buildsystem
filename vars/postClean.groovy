b19e6d7c9b9a7e6d2500d5fd3d08d89ae4a43fd9
Ryan Zoeller <ryan.zoeller@ni.com>
2018-10-25 08:18:59 -0500

    Create directory if it does not exist

--- vars/postClean.groovy
+++ vars/postClean.groovy
@@ -2,6 +2,7 @@ def call(){
     echo 'Kill VIPM so it cannot hold onto any file or folder handles'
     bat 'taskkill /IM "VI Package Manager.exe" /F /FI "STATUS eq RUNNING"'
     echo 'List everything in the build-temp directory (debug line to be removed)'
+    bat 'if not exist .\\build_temp mkdir .\\build_temp'
     bat 'dir .\\build_temp'
     echo 'Archiving all artifacts in the build-temp directory'
     archiveArtifacts artifacts: 'build_temp/**'
